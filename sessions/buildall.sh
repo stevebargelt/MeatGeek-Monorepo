99e88b06b8cca5b3a922ab6a662711484d55c71c
Steve Bargelt <steve@bargelt.com>
2021-06-08 06:51:57 -0700

    Added buildall.sh to sessions

---
+++ sessions/buildall.sh
@@ -0,0 +1,4 @@
+dotnet build src/MeatGeek.Sessions.Services/MeatGeek.Sessions.Services.csproj
+dotnet build src/MeatGeek.Sessions.WorkerApi/MeatGeek.Sessions.WorkerApi.csproj
+dotnet build src/MeatGeek.Sessions.Api/MeatGeek.Sessions.Api.csproj
+dotnet build src/MeatGeek.Sessions.Services.Tests/MeatGeek.Sessions.Services.Tests.csproj
