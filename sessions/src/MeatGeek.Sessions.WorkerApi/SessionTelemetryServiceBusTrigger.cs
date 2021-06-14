2f2952aa6b430f9718d807e5775afec117fb605f
Steve Bargelt <steve@bargelt.com>
2021-06-14 05:34:32 -0700

    Writing the device telemetry to the Sessions collection

--- sessions/src/MeatGeek.Sessions.WorkerApi/SessionTelemetryServiceBusTrigger.cs
+++ sessions/src/MeatGeek.Sessions.WorkerApi/SessionTelemetryServiceBusTrigger.cs
@@ -1,35 +1,77 @@
-// Default URL for triggering event grid function in the local environment.
-// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
-
-// using MeatGeek.Shared;
-// using MeatGeek.Shared.EventSchemas.Sessions;
+using Microsoft.Azure.Cosmos;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+using MeatGeek.Sessions.WorkerApi.Models;
 
 namespace MeatGeek.Sessions.WorkerApi
 {
     public class SessionTelemetryServiceBusTrigger
     {
+        private readonly CosmosClient _cosmosClient;
+
+        public SessionTelemetryServiceBusTrigger(CosmosClient cosmosClient)
+        {
+            _cosmosClient = cosmosClient;
+        }
 
         [FunctionName("SessionTelemetryServiceBusTrigger")]
-        public static void Run(
+        public static async Task Run(
             [ServiceBusTrigger("sessiontelemetry", "sessiontelemetry", Connection = "SessionsServiceBus")] 
-            string myTopicItem,
+            SmokerStatus[] smokerStatuses,
             Int32 deliveryCount,
             DateTime enqueuedTimeUtc,
             string messageId,
+            [CosmosDB(
+                databaseName: "Sessions",
+                collectionName: "sessions",
+                ConnectionStringSetting = "CosmosDBConnection")]
+            IAsyncCollector<SmokerStatus> smokerStatusOut,            
             ILogger log)
         {
-            log.LogInformation("SessionTelemetryServiceBusTrigger Called");
-            log.LogInformation($"Processed message: {myTopicItem}");
-            log.LogInformation($"EnqueuedTimeUtc={enqueuedTimeUtc}");
-            log.LogInformation($"DeliveryCount={deliveryCount}");
-            log.LogInformation($"MessageId={messageId}");
+
+            var exceptions = new List<Exception>();
+            log.LogInformation($"IoT Hub trigger function processing {smokerStatuses.Length} events.");
+
+            foreach (var smokerStatus in smokerStatuses)
+            {
+                try
+                {
+                    //var messageBody = Encoding.UTF8.GetString(smokerStatusData.Body.Array, smokerStatusData.Body.Offset, smokerStatusData.Body.Count);
+                    var smokerStatusString = JsonConvert.SerializeObject(smokerStatus);
+                    log.LogInformation($"SmokerStatus: {smokerStatusString}"); 
+                    log.LogInformation($"SmokerID: {smokerStatus.SmokerId}");
+                    if (smokerStatus.ttl is null || smokerStatus.ttl == 0 || smokerStatus.ttl == -1) {
+                        smokerStatus.ttl = 60 * 60 * 24 * 3;
+                    }
+                    smokerStatus.Type = "status";
+                    await smokerStatusOut.AddAsync(smokerStatus);
+                }
+                catch (Exception e)
+                {
+                    // We need to keep processing the rest of the batch - capture this exception and continue.
+                    // Also, consider capturing details of the message that failed processing so it can be processed again later.
+                    exceptions.Add(e);
+                }
+            }
+ 
+            // Once processing of the batch is complete, if any messages in the batch failed processing throw an exception so that 
+            //      there is a record of the failure.
+            if (exceptions.Count > 1)
+                throw new AggregateException(exceptions);
+
+            if (exceptions.Count == 1)
+                throw exceptions.Single();            
+
+            // log.LogInformation("SessionTelemetryServiceBusTrigger Called");
+            // log.LogInformation($"EnqueuedTimeUtc={enqueuedTimeUtc}");
+            // log.LogInformation($"DeliveryCount={deliveryCount}");
+            // log.LogInformation($"MessageId={messageId}");
+
         }
              
     }
