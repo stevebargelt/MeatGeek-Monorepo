6c44f1168ec49846d9118c776593e8324271d511
Steve Bargelt <steve@bargelt.com>
2021-06-14 16:15:09 -0700

    standardizing JSON capitalization to camelCase

--- sessions/src/MeatGeek.Sessions.WorkerApi/Models/Temps.cs
+++ sessions/src/MeatGeek.Sessions.WorkerApi/Models/Temps.cs
@@ -1,11 +1,19 @@
+using System;
+using Newtonsoft.Json;
+
 namespace MeatGeek.Sessions.WorkerApi.Models
 {
     public class Temps
     {
+        [JsonProperty("grillTemp")] 
         public double GrillTemp { get; set; }
+        [JsonProperty("probe1Temp")] 
         public double Probe1Temp { get; set; }
+        [JsonProperty("probe2Temp")] 
         public double Probe2Temp { get; set; }
+        [JsonProperty("probe3Temp")] 
         public double Probe3Temp { get; set; }
+        [JsonProperty("probe4Temp")] 
         public double Probe4Temp { get; set; }
 
     }
