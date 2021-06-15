e47a37ca583904d28af933a82a40aea5802037d8
Steve Bargelt <steve@bargelt.com>
2021-06-15 06:16:13 -0700

    Updated JSON capitalization to camelCase

--- sessions/src/MeatGeek.Sessions.Services/Models/Response/SessionDetails.cs
+++ sessions/src/MeatGeek.Sessions.Services/Models/Response/SessionDetails.cs
@@ -7,15 +7,15 @@ namespace MeatGeek.Sessions.Services.Models.Response
     {
         [JsonProperty("id")] 
         public string Id { get; set; }
-        [JsonProperty("smokerid")] 
+        [JsonProperty("smokerId")] 
         public string SmokerId { get; set; }
         [JsonProperty("title")] 
         public string Title { get; set; }
         [JsonProperty("description")] 
         public string Description { get; set; }
-        [JsonProperty("starttime")] 
+        [JsonProperty("startTime")] 
         public DateTime? StartTime { get; set; }
-        [JsonProperty("endtime")]
+        [JsonProperty("endTime")]
         public DateTime? EndTime { get; set; }
         [JsonProperty("timestamp")]
         public DateTime TimeStamp { get; set; }    
