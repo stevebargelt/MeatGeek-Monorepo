d6824c3ff4aa6320c52ae0b5354461dbdd53e8d6
Steve Bargelt <steve@bargelt.com>
2022-01-18 16:55:09 -0800

    EndSession - updating Swagger/OpenAPI

--- sessions/src/MeatGeek.Sessions.Api/EndSession.cs
+++ sessions/src/MeatGeek.Sessions.Api/EndSession.cs
@@ -39,12 +39,11 @@ namespace MeatGeek.Sessions
         [OpenApiOperation(operationId: "EndSession", tags: new[] { "session" }, Summary = "Ends an existing session.", Description = "Ends a session (sessions are 'cooks' or BBQ sessions).", Visibility = OpenApiVisibilityType.Important)]
         [OpenApiParameter(name: "smokerId", In = ParameterLocation.Path, Required = true, Type = typeof(string), Summary = "the Smoker Id", Description = "The Smoker Id", Visibility = OpenApiVisibilityType.Important)]
         [OpenApiParameter(name: "id", In = ParameterLocation.Path, Required = true, Type = typeof(Guid), Summary = "SessionID", Description = "The ID of the Session to end (GUID).", Visibility = OpenApiVisibilityType.Important)]
-        [OpenApiRequestBody(contentType: "application/json", bodyType: typeof(SessionDetails), Required = true, Description = "Session object with EndTime")]
-        [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.OK, Summary = "Session ended", Description = "Session Ended")]
+        [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.NoContent, Summary = "Session ended as requested.", Description = "Session Ended as requested.")]
         [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.MethodNotAllowed, Summary = "Invalid input", Description = "Invalid input")]         
         [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.NotFound, Summary = "Session not found", Description = "Session Not Found")]         
         [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Summary = "Invalid input", Description = "Invalid input")]
-        [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.InternalServerError, Summary = "An exception occurred", Description = "An exception occurred.")]
+        [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.InternalServerError, Summary = "An exception or internal server error occurred", Description = "An exception or internal server error occurred.")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "patch", "put", Route = "endsession/{smokerId}/{id}")] HttpRequest req, 
                 ILogger log,
