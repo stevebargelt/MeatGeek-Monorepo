b6e7483fb23b478c839ea993386c119173dbb66c
Steve Bargelt <steve@bargelt.com>
2022-01-18 18:02:00 -0800

    Updating OpenAPI/Swagger

--- sessions/src/MeatGeek.Sessions.Api/GetAllSessionStatuses.cs
+++ sessions/src/MeatGeek.Sessions.Api/GetAllSessionStatuses.cs
@@ -11,17 +11,11 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.OpenApi.Models;
 
 
 using MeatGeek.Sessions.Services;
-using MeatGeek.Sessions.Services.Models;
-using MeatGeek.Sessions.Services.Converters;
 using MeatGeek.Sessions.Services.Models.Data;
-using MeatGeek.Sessions.Services.Models.Request;
-using MeatGeek.Sessions.Services.Models.Results;
-using MeatGeek.Sessions.Services.Models.Response;
-using MeatGeek.Sessions.Services.Repositories;
-using MeatGeek.Shared;
 
 #nullable enable
 namespace MeatGeek.Sessions
@@ -41,6 +35,8 @@ namespace MeatGeek.Sessions
 
         [FunctionName("GetAllSessionStatuses")]
         [OpenApiOperation(operationId: "GetAllSessionStatuses", tags: new[] { "sessionstatus" }, Summary = "Returns all session statuses", Description = "Returns all statues for a given session.", Visibility = OpenApiVisibilityType.Important)]
+        [OpenApiParameter(name: "smokerid", In = ParameterLocation.Path, Required = true, Type = typeof(string), Summary = "The ID of the Smoker the session belings to", Description = "The ID of the Smoker the session belings to", Visibility = OpenApiVisibilityType.Important)]
+        [OpenApiParameter(name: "id", In = ParameterLocation.Path, Required = true, Type = typeof(string), Summary = "ID of the Session to return", Description = "The ID of the session to return", Visibility = OpenApiVisibilityType.Important)]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(SessionStatuses), Summary = "successful operation", Description = "successful response")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, contentType: "application/json", bodyType: typeof(string), Summary = "Invalid input", Description = "Invalid input")]
         [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.NotFound, Summary = "Session Statuses not found", Description = "Session Statuses Not Found")]         
--- sessions/src/MeatGeek.Sessions.Api/GetSessionById.cs
+++ sessions/src/MeatGeek.Sessions.Api/GetSessionById.cs
@@ -8,19 +8,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 
 
 using MeatGeek.Sessions.Services;
-using MeatGeek.Sessions.Services.Models;
-using MeatGeek.Sessions.Services.Converters;
-using MeatGeek.Sessions.Services.Models.Request;
-using MeatGeek.Sessions.Services.Models.Results;
 using MeatGeek.Sessions.Services.Models.Response;
-using MeatGeek.Sessions.Services.Repositories;
-using MeatGeek.Shared;
 
 using Microsoft.OpenApi.Models;
 
@@ -42,8 +35,8 @@ namespace MeatGeek.Sessions
 
         [FunctionName("GetSessionById")]
         [OpenApiOperation(operationId: "GetSessionById", tags: new[] { "session" }, Summary = "Find Session by ID", Description = "Returns a single session. Sessions are cooking / BBQ Sessions or cooks.", Visibility = OpenApiVisibilityType.Important)]
-        [OpenApiParameter(name: "smokerid", In = ParameterLocation.Query, Required = true, Type = typeof(string), Summary = "The ID of the Smoker the session belings to", Description = "The ID of the Smoker the session belings to", Visibility = OpenApiVisibilityType.Important)]
-        [OpenApiParameter(name: "id", In = ParameterLocation.Query, Required = true, Type = typeof(string), Summary = "ID of the Session to return", Description = "The ID of the session to return", Visibility = OpenApiVisibilityType.Important)]
+        [OpenApiParameter(name: "smokerid", In = ParameterLocation.Path, Required = true, Type = typeof(string), Summary = "The ID of the Smoker the session belings to", Description = "The ID of the Smoker the session belings to", Visibility = OpenApiVisibilityType.Important)]
+        [OpenApiParameter(name: "id", In = ParameterLocation.Path, Required = true, Type = typeof(string), Summary = "ID of the Session to return", Description = "The ID of the session to return", Visibility = OpenApiVisibilityType.Important)]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(SessionDetails), Summary = "successful operation", Description = "successful response")]
         [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Summary = "Invalid ID supplied", Description = "Invalid ID supplied")]
         [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.NotFound, Summary = "Session not found", Description = "Session not found")]
