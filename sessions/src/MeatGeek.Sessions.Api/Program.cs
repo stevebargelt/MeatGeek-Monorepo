0273ffab73db35f86d2993156f45225e7980acbc
Steve Bargelt <steve@bargelt.com>
2025-08-13 17:14:35 -0700

    Iso fixes

--- sessions/src/MeatGeek.Sessions.Api/Program.cs
+++ sessions/src/MeatGeek.Sessions.Api/Program.cs
@@ -14,9 +14,9 @@ using MeatGeek.Shared;
 var host = new HostBuilder()
     .ConfigureHostConfiguration(configHost =>
     {
-        // configHost.SetBasePath(Environment.CurrentDirectory);
-        // configHost.AddEnvironmentVariables();
-        // configHost.AddJsonFile("local.settings.json", optional: true, reloadOnChange: true);
+        configHost.SetBasePath(Environment.CurrentDirectory);
+        configHost.AddEnvironmentVariables();
+        configHost.AddJsonFile("local.settings.json", optional: true, reloadOnChange: true);
     })
     .ConfigureAppConfiguration(configBuilder =>
     {
@@ -24,29 +24,29 @@ var host = new HostBuilder()
     })        
     .ConfigureServices(services =>
     {
-        // var appInsightsConnectionString = Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING");
-        // if (string.IsNullOrEmpty(appInsightsConnectionString))
-        // {
-        //     throw new ArgumentNullException("Please specify a value for APPLICATIONINSIGHTS_CONNECTION_STRING in the local.settings.json file or your Azure Functions Settings.");
-        // }
-        // services.AddApplicationInsightsTelemetryWorkerService();
-        // services.ConfigureFunctionsApplicationInsights();
-        // services.AddSingleton<CosmosClient>((s) =>
-        // {
-        //     var connectionString = Environment.GetEnvironmentVariable("CosmosDBConnection");
-        //     if (string.IsNullOrEmpty(connectionString))
-        //     {
-        //         throw new ArgumentNullException("Please specify a value for CosmosDBConnection in the local.settings.json file or your Azure Functions Settings.");
-        //     }
+        var appInsightsConnectionString = Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING");
+        if (string.IsNullOrEmpty(appInsightsConnectionString))
+        {
+            throw new ArgumentNullException("Please specify a value for APPLICATIONINSIGHTS_CONNECTION_STRING in the local.settings.json file or your Azure Functions Settings.");
+        }
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+        services.AddSingleton<CosmosClient>((s) =>
+        {
+            var connectionString = Environment.GetEnvironmentVariable("CosmosDBConnection");
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentNullException("Please specify a value for CosmosDBConnection in the local.settings.json file or your Azure Functions Settings.");
+            }
 
-        //     var cosmosDbConnectionString = new CosmosDbConnectionString(connectionString);
-        //     return new CosmosClientBuilder(cosmosDbConnectionString.ServiceEndpoint?.OriginalString ?? throw new ArgumentNullException(nameof(connectionString), "ServiceEndpoint is null"), cosmosDbConnectionString.AuthKey)
-        //         .WithBulkExecution(true)
-        //         .Build();
-        // });
-        // services.AddScoped<ISessionsService, SessionsService>();
-        // services.AddScoped<ISessionsRepository, SessionsRepository>();
-        // services.AddScoped<IEventGridPublisherService, EventGridPublisherService>();
+            var cosmosDbConnectionString = new CosmosDbConnectionString(connectionString);
+            return new CosmosClientBuilder(cosmosDbConnectionString.ServiceEndpoint?.OriginalString ?? throw new ArgumentNullException(nameof(connectionString), "ServiceEndpoint is null"), cosmosDbConnectionString.AuthKey)
+                .WithBulkExecution(true)
+                .Build();
+        });
+        services.AddScoped<ISessionsService, SessionsService>();
+        services.AddScoped<ISessionsRepository, SessionsRepository>();
+        services.AddScoped<IEventGridPublisherService, EventGridPublisherService>();
     })
     .ConfigureFunctionsWebApplication()
         // Must be set at the end of the chain
